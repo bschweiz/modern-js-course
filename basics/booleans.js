c2612878db60c44d8b19616e7e9e98e534121bfc
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-12 15:48:09 -0500

    still too easy

--- basics/booleans.js
+++ basics/booleans.js
@@ -1,9 +1,9 @@
-let age = 4
+let age = 74
 const isChild = age <= 7
 const isSenior = age >= 65
 
-console.log(isChild, isSenior);
+console.log(isChild, isSenior)
 
 if (isChild) {
     console.log('You are a baby')
-}
+} else if (isSenior) {console.log('You are Old')}
