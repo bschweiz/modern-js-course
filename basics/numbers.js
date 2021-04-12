e5218613272a889d0839212aa4d5fdef291a62d9
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-12 15:57:41 -0500

    else if statements

--- basics/numbers.js
+++ basics/numbers.js
@@ -3,12 +3,18 @@
 
 // console.log(`your score is ${100*(studentScore/max)}%`)
 
-let f = 28
+let f = 35
 let c = (f - 32)*(5/9)
 let k = (f + 459.67) * 5/9
 
 console.log(f,c,k)
 
 let isFreezing = f < 32
+let isHOT = f > 100
 
 console.log(isFreezing)
+
+if (isFreezing) {
+    console.log("it's FREEZING");
+} else if (isHOT) {console.log('it is so HOT')}
+else {console.log("go for it!");}
