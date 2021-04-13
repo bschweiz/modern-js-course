55bcd73edc58c72f49e6877a9c77ac2aa3d0a968
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-13 16:48:29 -0500

    letter variable reassignment not working

--- basics/grade-calculator.js
+++ basics/grade-calculator.js
@@ -3,6 +3,21 @@
 //  you got  a (letter grade) (percentage)
 
 let gradeCalc = (student, max) => {
-    let letter =
-    let percentage = 
+    let percentage = student/max 
+    let letter
+
+    if (percentage >= 90) {
+        let letter = 'A' 
+    } else if (percentage >= 80) {
+        let letter = 'B' 
+    } else if (percentage >= 70) {
+        
+        let letter = 'C' 
+    } else if (percentage >= 60) {
+        let letter = 'D'
+    }
+
+    console.log(`You got a ${letter} (${percentage}%)`)
 }
+
+gradeCalc(67,75)
