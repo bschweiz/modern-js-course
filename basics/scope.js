9a313c049e89d33645fca0d0d433840120e91ec5
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-13 16:33:30 -0500

    format strings now

--- basics/scope.js
+++ basics/scope.js
@@ -1,2 +1,16 @@
-let var1 = 'variable uno'
+// Global scope (convertF2C, t1, t2)
+    // Local scope (f,c)
 
+let convertF2C = (f = 74) =>  {
+    let c = (f - 32) * 5/9
+
+    if (c <= 0) {
+        let isFreezing = true
+    } 
+
+    return c
+}
+
+
+
+console.log(convertF2C());
