d5336a0cb070dbd4f8dfb29e9ad257ee89658b28
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-13 16:27:05 -0500

    default arugments

--- basics/arguments.js
+++ basics/arguments.js
@@ -3,3 +3,17 @@ let cubed = (a,b,c) => {
 }
 
 console.log(cubed(7,2,3))
+
+let getScore = (_name = 'nobody', score = 0) => {
+    return 'Name:  ' + _name + ' - Score: ' + score
+}
+
+let score = getScore()
+
+console.log(score);
+
+let getTip = (total, tipPercent = .2) {
+    return total * tipPercent
+}
+
+console.logt(getTip(100))
