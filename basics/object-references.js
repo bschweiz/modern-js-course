0fa5a27a317293b10855e75f0b9f96a6247b9730
Benjamin Schweizer <bschweiz@gmail.com>
2021-04-14 11:35:46 -0500

    passed testing

--- basics/object-references.js
+++ basics/object-references.js
@@ -6,7 +6,19 @@ let myAcct = {
 
 let addExpense = (account, amount) => {
     account.expenses = account.expenses + amount
-    console.log(`Account for ${account.name}, ${account.income} in income. Expenses of ${myAcct.expenses}`)
+}
+let addIncome = (income, amount) => {
+    income.expenses = income.expenses + amount
 }
 
+let resetAccount = (account) => {
+    account.expenses = 0
+    account.income = 0
+}
+
+let getAcctSummary = (account) => {
+    let balance = account.income - account.expenses
+    return `Account for ${account.name}, balance of ${balance}, ${account.income} in income. Expenses of ${myAcct.expenses}`
+} 
 addExpense(myAcct, 3.03)
+console.log(getAcctSummary(myAcct))
